f9692baedab263b3bf742bec5c301ea1b1dbd904
Muppu Kokila <127586949+Muppukokila@users.noreply.github.com>
2025-11-28 19:59:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_community.vectorstores import Chroma
-from langchain.chains import RetrievalQA
+from langchain_experimental.chat_models import RetrievalQA
 from langchain.prompts import PromptTemplate
 
 # --- Streamlit Page ---
